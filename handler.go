e145abd074298a342b6e8e67da9ce350b6bd6e90
sasatake <t06418as@gmail.com>
2022-10-16 08:03:36 +0900

    response to json

--- handler.go
+++ handler.go
@@ -1,25 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 )
 
+type Response struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+}
+
+func createResponseJson(status int, message string) string {
+	response := Response{Status: status, Message: message}
+	responseJsonBytes, err := json.MarshalIndent(response, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+		return err.Error()
+	}
+	return string(responseJsonBytes)
+}
+
 func Index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, createResponseJson(http.StatusNotFound, "not found."))
 		return
 	}
 	name := r.URL.Query().Get("name")
 
 	if name == "" {
 		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, "set name parameter.")
+		fmt.Fprint(w, createResponseJson(http.StatusBadRequest, "set name parameter."))
 		return
 	}
 
-	message := fmt.Sprintf("Hello %s\n", name)
-	log.Printf("parameter name value is %s", name)
-	fmt.Fprint(w, message)
+	message := fmt.Sprintf("Hello %s", name)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, createResponseJson(http.StatusOK, message))
 }
